449d7e9a6fe70af4cafd1e7316dfa69dfad8162a
Paul Schmitt <paul.schmitt@gmail.com>
2023-02-02 19:15:47 -1000

    dns plotter

--- plot_dns.py
+++ plot_dns.py
@@ -1,4 +1,5 @@
 import matplotlib
+
 matplotlib.use('Agg')
 
 from collections import Counter
@@ -7,9 +8,19 @@ import glob
 import seaborn as sns
 from matplotlib import pyplot as plt
 import pickle
+import numpy as np
 
 relay = []
 norelay = []
+google = []
+cf = []
+q9 = []
+
+
+def cdf(x, plot=True, *args, **kwargs):
+  x, y = sorted(x), np.arange(len(x)) / len(x)
+  return plt.plot(x, y, *args, **kwargs) if plot else (x, y)
+
 
 with open('rtts_norelay.pickle', 'rb') as handle:
   norelaydict = pickle.load(handle)
@@ -17,16 +28,42 @@ with open('rtts_norelay.pickle', 'rb') as handle:
 with open('rtts_relay.pickle', 'rb') as handle:
   relaydict = pickle.load(handle)
 
-# print(relaydict)
+with open('rtts_google.pickle', 'rb') as handle:
+  googledict = pickle.load(handle)
+
+with open('rtts_cf.pickle', 'rb') as handle:
+  cfdict = pickle.load(handle)
+
+with open('rtts_q9.pickle', 'rb') as handle:
+  q9dict = pickle.load(handle)
+
 for key, val in relaydict.items():
-  relay.append(val)
+  relay.append(float(val))
 
 for key, val in norelaydict.items():
-  norelay.append(val)
+  norelay.append(float(val))
 
-sns.kdeplot(data=norelay, cumulative=True, label="No Relay")
-sns.kdeplot(data=relay, cumulative=True, label="Relay")
-# plt.xlim(0, 26000)
+for key, val in googledict.items():
+  google.append(float(val))
+
+for key, val in cfdict.items():
+  cf.append(float(val))
+
+for key, val in q9dict.items():
+  q9.append(float(val))
+
+print(np.median(norelay))
+print(np.median(relay))
+
+cdf(relay, label="relay")
+cdf(norelay, label="no relay")
+cdf(google, label="Google")
+cdf(cf, label="Cloudflare")
+cdf(q9, label="Quad9")
+
+plt.xlim(0, 1500)
+plt.yticks([0, .25, .5, .75, 1])
+plt.xscale('symlog')
 plt.xlabel('DNS resolution time (ms)')
 plt.grid()
 plt.legend()
